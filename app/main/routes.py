cfd1eb3aa88c015fc895f64b6cb8d73d3bb9f9fa
errantsniper <jacob.niese@gmail.com>
2020-04-21 10:17:32 -0400

    Hello_World
    Basic 'Hello World' service established.

---
+++ app/main/routes.py
@@ -0,0 +1,11 @@
+from flask import render_template, flash, redirect, url_for, request, g, jsonify, current_app
+
+# from app import db
+from app.main import bp
+# from app.models import User, Message, Notification
+
+
+@bp.route('/')
+@bp.route('/index')
+def index():
+    return 'Hello World'
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from flask import current_app, url_for
+from flask_login import UserMixin
+# from app import db
+
+
+# class User(UserMixin, db.Model):
+#     pass
+
+
+# class Message(db.Model):
+#     pass
+
+
+# class Notification(db.Model):
+#     pass
