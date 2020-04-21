197a12c7c330207c551dc40e0dfcea5b6a0ca3fc
errantsniper <jacob.niese@gmail.com>
2020-04-21 11:38:39 -0400

    Complete
    Task completed.

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from flask import current_app, url_for
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from time import time
+import json
 from app import db
 
 
@@ -23,6 +25,7 @@ class User(UserMixin, db.Model):
         backref="recipient",
         lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -32,6 +35,18 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self)\
+            .filter(Message.timestamp > last_read_time)\
+            .count()
+
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
 
 
 class Message(db.Model):
@@ -43,3 +58,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return f'<Message {self.body}>'
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
