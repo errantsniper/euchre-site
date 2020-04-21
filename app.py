c5129742dd2922f6953d1e9708052430c21768c5
errantsniper <jacob.niese@gmail.com>
2020-04-21 10:32:39 -0400

    Complete
    Basic User Model Complete

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from app import create_app
-#from app.models import User, Message, Notification
+from app import create_app, db
+from app.models import User
 
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    #return {'db': db, 'User': User, 'Message': Message, 'Notification': Notification}
-    pass
+    return {'db': db, 'User': User}
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request, g, jsonify, current_app
 
-# from app import db
+from app import db
 from app.main import bp
-# from app.models import User, Message, Notification
+from app.models import User
 
 
 @bp.route('/')
